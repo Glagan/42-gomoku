1a60046635cac10b28a6f5523f1a8fb1b9c172b2
Glagan <8447621+Glagan@users.noreply.github.com>
2022-05-19 16:51:11 +0200

    Add PvA and PvP

--- src/main.rs
+++ src/main.rs
@@ -33,8 +33,6 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let rules = RuleSet::default();
-    let mut board = Board::default();
     // println!("{}", board);
     // println!("---");
     // let player1 = Player::Black;
@@ -76,12 +74,13 @@ async fn main() {
             game_selector(&mut game);
         } else {
             // Draw game
-            draw_goban(&board);
+            draw_goban(&game.board);
             display_panel_text(&mut game);
             draw_current_rock(&game);
 
-            // Handle Input
+            // Handle Input based on current game mode
             if game.mode != GameMode::AvA {
+                // Player play
                 if is_mouse_button_released(MouseButton::Left) {
                     b_mouse_pressed = false;
                 } else if is_mouse_button_down(MouseButton::Left)
@@ -96,9 +95,12 @@ async fn main() {
                     {
                         let rock_x = mouse_x as usize / SQUARE_SIZE;
                         let rock_y = mouse_y as usize / SQUARE_SIZE;
-                        if board.pieces[Board::coordinates_to_index(rock_x, rock_y)] == Pawn::None {
-                            board.set_move(
-                                &rules,
+                        println!("checking {} {}", rock_x, rock_y);
+                        if game.board.pieces[Board::coordinates_to_index(rock_x, rock_y)]
+                            == Pawn::None
+                        {
+                            game.board.set_move(
+                                &game.rules,
                                 &Move {
                                     index: Board::coordinates_to_index(rock_x, rock_y),
                                     player: game.current_player,
@@ -106,6 +108,32 @@ async fn main() {
                             );
                             game.next_player();
                         }
+                    }
+                }
+                // Computer Play
+                if game.mode == GameMode::PvA && game.current_player == Player::Black {
+                    let play_result =
+                        game.computer
+                            .play(&game.rules, &game.board, 4, &game.current_player);
+                    println!("computer play: {:#?}", play_result);
+                    if let Ok(play) = play_result {
+                        if let Some(movement) = play.movement {
+                            game.board.set_move(&game.rules, &movement);
+                            game.next_player();
+                        }
+                    }
+                }
+            }
+            // AvA just play in turn, no input
+            else {
+                let play_result =
+                    game.computer
+                        .play(&game.rules, &game.board, 4, &game.current_player);
+                println!("computer play: {:#?}", play_result);
+                if let Ok(play) = play_result {
+                    if let Some(movement) = play.movement {
+                        game.board.set_move(&game.rules, &movement);
+                        game.next_player();
                     }
                 }
             }
